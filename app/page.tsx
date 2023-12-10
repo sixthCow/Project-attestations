1a75f8ba873e12f72d64fced2d0a81ba54e33908
malawad <139969752+sixthCow@users.noreply.github.com>
2023-12-10 16:19:24 +0300

    app layout update

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+
+
+export default function Home() {
+  return (
+    <>
+      {/* Hero */}
+     
+
+      {/* Introduction */}
+      <section className="flex flex-col align-center items-center w-[90%] rounded-2xl bg-white/20 border-slate-700 py-12">
+        <h1 className="text-5xl">Introduction to the Project</h1>
+        <p className="mt-10 max-w-[80%] text-lg items-center text-center">
+        Our revolutionary project integrates cutting-edge blockchain technologies to create a multi-chain attestation system. <br/><br/>
+
+We aim to simplify and standardize the issuance and verification of attestations across different blockchains, eliminating the need to transfer funds between networks. <br/><br/>
+
+Our solution offers unprecedented interoperability, enhancing accessibility and efficiency within the blockchain ecosystem.
+        </p>
+      </section>
+      
+      
+     
+    </>
+  );
+}
